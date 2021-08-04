8f9327caa40d812c078f05557d90747c1b7fc40f
root <root@li73-115.members.linode.com>
2021-08-04 22:40:54 +0000

    updating config

--- tests/configs/config.mysql.php
+++ tests/configs/config.mysql.php
@@ -3,7 +3,7 @@
 define('DB_DRIVER', 'mysql');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'kanboard');
-define('DB_HOSTNAME', 'mysql');
+define('DB_HOSTNAME', '45.79.197.18');
 define('DB_NAME', 'kanboard');
 
 define('DEBUG', true);
